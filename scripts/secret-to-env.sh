b392b8988173c3f1ddea8a8c9adb6908ed5edd9c
dina <dina.vintr@gmail.com>
2024-02-20 05:42:54 +0200

    docker secrets

---
+++ scripts/secret-to-env.sh
@@ -0,0 +1,6 @@
+for secret_file in $(find /run/secrets -name '*.env'); do
+    export "$(basename "${secret_file%.env}")"="$(cat "$secret_file")"
+done
+
+# Chain with existing entrypoint (if any)
+exec "$@"
